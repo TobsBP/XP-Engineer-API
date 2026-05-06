2a6d1d5c922573786007c4b5d60ce59c1f7313cc
Tobias <tobias.bp2004@gmail.com>
2026-05-06 14:28:07 -0300

    fix(type): schema zod

--- src/models/lessons/application-item.schema.ts
+++ src/models/lessons/application-item.schema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const ApplicationItemSchema = z.object({
-	id: z.string().uuid(),
+	id: z.uuid(),
 	lesson_id: z.number().int(),
 	title: z.string().max(200),
 	description: z.string(),
